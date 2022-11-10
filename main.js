b3bdc0ec4daaabfea7940aba2fe34e30e0ceece2
krissy <kmujakic@gmail.com>
2022-11-10 21:14:11 +0100

    let and const exercise

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+
+// Quiz
+
+// What is the difference between var and let?
+// The var variable is set to the window object, so we can easily overwrite some existing properties of the window object by naming the var variable with this name. We can declare a var variable with the same name more than once, while we are not allowed to do so with let.
+
+
+// What is the difference between var and const?
+// With the const variable, we cannot change its value.
+
+// What is the difference between let and const?
+// We can reassign a value with the let variable, while const does not allow us to do so.
+
+// What is hoisting?
+// Hoisting is an operation where variables are first declared when the code runs and then the values are initialized. Var variable is being hoisted, while let and const aren’t because they create a block scope.
+
